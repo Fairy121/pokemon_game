a72cd37d4be42f7c57f19b89edb0debc52efb535
Rachael A. Concessio <RachaelConcessio@gmail.com>
2021-02-10 12:54:30 -0500

    first commit

---
+++ src/Globals/Styles/misc.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import {Base} from './main';
+
+export const Button = styled(Base).attrs({
+    as: "button"
+  })`
+   border-style:${({variant,border}) => 
+  variant === 'outlined' ? border : 'none'};
+   background:${({variant,fill}) => 
+    variant === 'fill' ? fill : 'none'
+   }
+   
+`;
+
+export const Input = styled(Base).attrs({
+    as:'input'
+})`
+    border-style:none;
+    outline:none;
+    width:${props => props.width ? props.width : '100%'};
+    &:not(:first-child):{
+        margin-top:16px;
+    }
+`
